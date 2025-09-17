36416199a35c5681d82076b502d1749913f42f3c
sangeethpromod <sangeethpromod10@gmail.com>
2025-09-17 23:48:20 +0530

    structured files

--- src/routes/githubPatRoutes.ts
+++ src/routes/githubPatRoutes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
-import { savePAT, getRepos, getRepoContents } from '../controllers/github/githubPatController';
+import { savePAT, getRepos, getRepoContents, getFileContent } from '../controllers/github/githubPatController';
 
 const router = Router();
 
 router.post('/save-pat', savePAT);
 router.get('/repos/:email', getRepos);
+
+//API to get the content of a specific file in a repo
+router.get('/repos/:email/:owner/:repo/file/*/:filename', getFileContent);
+
 router.get('/repos/:email/:owner/:repo/contents/*', getRepoContents);
 
 export default router;
